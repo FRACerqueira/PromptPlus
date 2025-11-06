2527569d2776ff83c4e0b9267503edea85f71893
Fernando Cerqueira <frcerqueira@LIVE.COM>
2025-11-06 18:28:55 -0300

    Code cleanup, namespace correction, addition of more examples. Documentation review and screenshots of the images.

--- samples/ConsoleAlternateScreenSamples/Program.cs
+++ samples/ConsoleAlternateScreenSamples/Program.cs
@@ -3,7 +3,6 @@
 // The maintenance and evolution is maintained by the PromptPlus project under MIT license
 // ***************************************************************************************
 
-
 using PromptPlusLibrary;
 
 namespace ConsoleAlternateScreenSamples
